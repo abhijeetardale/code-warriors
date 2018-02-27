a09f3a3a626ea0e4ac60fece99e9983d2d4b77ac
abhijeetardale <abhijeetardale@github.com>
2018-02-27 11:07:29 +0000

    initial commit

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,43 @@
+package controllers
+
+import java.util
+import javax.inject._
+
+import play.Routes
+import play.api._
+import play.api.i18n.MessagesApi
+import play.api.mvc._
+import play.api.libs.json.Json
+import play.api.libs.ws.WSClient
+import play.api.mvc.Controller
+import play.api.routing.JavaScriptReverseRouter
+import play.modules.reactivemongo.ReactiveMongoApi
+import reactivemongo.api.{MongoConnection, MongoDriver, DefaultDB}
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+import play.api.mvc._
+import play.api.libs.ws._
+import play.api.http.HttpEntity
+
+
+import javax.inject.Inject
+
+import play.api._
+import play.api.mvc._
+
+import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success, Try}
+
+
+@Singleton
+class HomeController @Inject() (ws: WSClient)(implicit val messagesApi: MessagesApi, context: ExecutionContext) extends Controller with i18n.I18nSupport {
+
+
+  def index = Action { implicit request =>
+    Ok(views.html.index())
+  }
+}
+
+
